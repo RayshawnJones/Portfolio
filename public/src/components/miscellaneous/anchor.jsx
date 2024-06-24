c58313990d6b2986277e76edc10e5a1372e6c1fe
R.Jones <RayshawnJones1991@gmail.com>
2024-06-24 12:50:25 -0400

    push

--- public/src/components/miscellaneous/anchor.jsx
+++
@@ -1,7 +0,0 @@
-export default function Anchor({ children, link, classes = "button" }) {
-  return (
-    <a className={classes} href={link}>
-      <span>{children}</span>
-    </a>
-  );
-}
--- public/src/components/miscellaneous/button.jsx
+++
@@ -1,7 +0,0 @@
-export default function Button({ children, onClick, classes = "button" }) {
-  return (
-    <button className={classes} onClick={onClick}>
-      <span>{children}</span>
-    </button>
-  );
-}
